dd97ef8de98410317438b054e46715a9888cd1cc
Jilroge7 <1672@holbertonschool.com>
2021-12-01 04:19:00 +0000

    eslint

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -2,7 +2,7 @@ const { Seq } = require('immutable');
 
 function printBestStudents(obj) {
   const studentSeq = Seq(obj)
-    .filter((x) => x.score >= 70);
+    .filter((x) => x.score >= 70)
     .map((x) => x.toUpperCase());
   console.log(studentSeq.toObject());
 }
