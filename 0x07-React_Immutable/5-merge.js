de5c3b0502fd202e1e26028905f4414408fd0225
Jilroge7 <1672@holbertonschool.com>
2021-12-01 03:26:23 +0000

    adding tsk 5

---
+++ 0x07-React_Immutable/5-merge.js
@@ -0,0 +1,16 @@
+const { List, Map } = require('immutable');
+
+function concatElements(page1, page2) {
+  const pageOne = List(page1);
+  const pageTwo = List(page2);
+  const pageList = pageOne.concat(pageTwo);
+  return (pageList);
+}
+
+function mergeElements(page1, page2) {
+  const pageOne = Map(page1);
+  const pageTwo = Map(page2);
+  const pageList = pageOne.merge(pageTwo);
+}
+
+module.exports = { concatElements, mergeElements};
