80f4811f9c115d6778f98dddd8e1cefc0a0f1f23
Jilroge7 <1672@holbertonschool.com>
2021-12-01 04:17:53 +0000

    adding tsk 8

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -1,9 +1,9 @@
 const { Seq } = require('immutable');
 
 function printBestStudents(obj) {
-  const studentSeq = Seq(obj);
-  .filter((x) => x.score >= 70);
-  .map((x) => x.toUpperCase());
+  const studentSeq = Seq(obj)
+    .filter((x) => x.score >= 70);
+    .map((x) => x.toUpperCase());
   console.log(studentSeq.toObject());
 }
 
