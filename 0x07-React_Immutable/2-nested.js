426e4240038fd1731e0ba268e648a702b2d07b11
Jilroge7 <1672@holbertonschool.com>
2021-12-01 02:09:44 +0000

    eslint fix

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -4,6 +4,6 @@ function accessImmutableObject(object, array) {
   const immutableObj = fromJS(object);
   const nestedVal = immutableObj.getIn(array);
   return (nestedVal);
-};
+}
 
 module.exports = accessImmutableObject;
