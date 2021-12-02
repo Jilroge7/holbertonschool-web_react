b88589b8d621e60aaaf153876298e6906134164f
Jilroge7 <1672@holbertonschool.com>
2021-12-02 04:48:49 +0000

    adding task 0, and readme

---
+++ 0x06-react_state/README.md
@@ -0,0 +1,34 @@
+# 0x06 - React State
+
+## Resources:mag_right::books:
+Read or watch:
+* [State and lifecycle](https://intranet.hbtn.io/rltoken/73x5hG08XwbZ_tKwrLVsaQ)
+* [SetState and State callback](https://intranet.hbtn.io/rltoken/leRP2NUTKuNQn9fjCkIIWQ)
+* [Context](https://intranet.hbtn.io/rltoken/rJiNSBnvbLhet4FgyWxrkg)
+* [Forms and Controlled components](https://intranet.hbtn.io/rltoken/lCiofK_nr9h_-bcST8x_Zg)
+* [Lifting State Up](https://intranet.hbtn.io/rltoken/vFgst4GPjbIENWQaVC0R3Q)
+* [React Hooks](https://intranet.hbtn.io/rltoken/7DD2jihrJyavY-ReXXIz_w)
+* [Enzyme State](https://intranet.hbtn.io/rltoken/5PP4QrClUCON8ivE83Wh7Q)
+* [Enzyme SetState](https://intranet.hbtn.io/rltoken/BLCWTFXnN8Oy4EBcLuIw8Q)
+* [Enzyme Instance](https://intranet.hbtn.io/rltoken/5PP4QrClUCON8ivE83Wh7Q)
+* [Enzyme Simulate](https://intranet.hbtn.io/rltoken/BLCWTFXnN8Oy4EBcLuIw8Q)
+
+
+---
+## Learning Objectives:elephant:🧠
+What you should learn from this project:
+
+* What the state of a component or a container is
+* The lifecycle of a component
+* How to modify a state and execute code in the right order
+* What a controlled component is
+* How to use Forms in React
+* How to reuse smaller components, keep them pure, and lift its state to principal containers
+* The use of a React Hook and how to create one
+* How to test State changes with Enzyme
+
+---
+---
+
+## Author:art:
+* **Jill Rogers** - [Jilroge7](https://github.com/Jilroge7/holbertonschool-web_react.git)
---
+++ 0x06-react_state/task_0/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,134 @@
+import React, {Component} from "react";
+import { StyleSheet, css } from 'aphrodite';
+import NotificationItem from './NotificationItem.js';
+import closeImg from '../assets/close-icon.png';
+import { getLatestNotification } from "../utils/utils.js";
+import PropTypes from 'prop-types';
+import NotificationItemShape from './NotificationItemShape.js';
+
+
+class Notifications extends React.Component {
+  constructor(props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+  shouldComponentUpdate(nextProps) {
+    return nextProps.length > this.props.listNotifications.length || nextProps.displayDrawer !== this.props.displayDrawer;
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  render() {
+    const { displayDrawer, listNotifications, handleDisplayDrawer, handleHideDrawer } = this.props;
+    return (
+      <div className={
+        displayDrawer
+          ? css(styles.container, styles.displayOn)
+          : css(styles.container)}
+      >
+        <div className={css(styles.menuItem)}
+          onClick={handleDisplayDrawer}
+          >Your notifications
+        </div>
+        {
+          displayDrawer && (
+          <div className={css(styles.notifications, styles.noBox)}>
+            <p className={css(styles.centerText)}>
+              Here is the list of notifications
+            </p>
+            <ul>
+              { listNotifications.length > 0
+                ? listNotifications.map(({ markAsRead, type, value, html, id }) => (
+                  <NotificationItem markAsRead={this.markAsRead} id={id} type={type} value={value} html={html} key={id} />
+                ))
+                : <NotificationItem value='No new notification for now' />
+              }
+            </ul>
+            <button className={css(styles.button)}
+              aria-label="Close"
+              onClick={handleHideDrawer}>
+              <img alt="close-icon" src={closeImg} width="10px" height="10px"/>
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+  handleHideDrawer: PropTypes.func,
+  handleDisplayDrawer: PropTypes.func
+};
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: [],
+  handleDisplayDrawer: () => {},
+  handleHideDrawer: () => {}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: '1rem',
+    marginRight: '1rem',
+    gridRow: '1'
+  },
+
+  notifications: {
+    border: 'dashed #ec4242',
+    padding: '0.40rem',
+    marginTop: '.3rem'
+  },
+
+  menuItem: {
+    textAlign: 'right',
+    marginRight: '0.5rem'
+  },
+
+  button: {
+    position: "absolute", 
+    top: "5.75rem",
+    right: "2.2rem",
+    '@media (max-width: 900px)': {
+      top: '19.75rem',
+      right: '4rem'
+    }
+  },
+
+  displayOn: {
+    '@media (max-width: 990px)': {
+      gridRow: '2'
+    }
+  },
+
+  noBox: {
+    '@media (max-width: 900px)': {
+      border: 'none',
+    }
+  },
+
+  centerText: {
+    '@media (max-width: 900px)': {
+      fontSize: '20px'
+    }
+  },
+
+  default: {
+    color: 'blue'
+  },
+
+  urgent: {
+    color: 'red'
+  },
+});
+
+
+
+export default Notifications;
