b470f68664ba39d713ee06e1e91146276ed80a9e
Jilroge7 <1672@holbertonschool.com>
2021-12-14 04:08:35 +0000

    adding task 6, incomplete

---
+++ 0x08_react_redux_action_creator_normalizr/task_6/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+class NotificationItem extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const {type, html, value, markAsRead, id } = this.props;
+    return (
+      <>
+        <li onClick={() => markAsRead(id)} className={type === 'default' ? css(styles.default) : css(styles.urgent)} data-notification-type={type} dangerouslySetInnerHTML={html}>{value}</li>
+      </>
+    );
+  }
+}
+
+NotificationItem.propTypes =  {
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  markAsRead: PropTypes.func,
+  id: PropTypes.number,
+  __html: PropTypes.shape({
+    html: PropTypes.string
+  })
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+  markAsRead: () => {}
+};
+
+const styles = StyleSheet.create({
+  default: {
+    color: 'blue',
+    '@media (max-width: 900px)': {
+      borderBottom: '1px solid black',
+      listStyle: 'none',
+      fontSize: '20px',
+      padding: '10px 8px'
+    }
+  },
+
+  urgent: {
+    color: 'red',
+    '@media (max-width: 900px)': {
+      borderBottom: '1px solid black',
+      listStyle: 'none',
+      fontSize: '20px',
+      padding: '10px 8px'
+    }
+  }
+})
+
+export default NotificationItem;
---
+++ 0x08_react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.test.js
@@ -0,0 +1,28 @@
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER} from './uiActionTypes.js';
+import { login, logout, displayNotificationDrawer, hideNotificationDrawer} from './uiActionCreators.js';
+
+describe('returns correct description when action is created', () => {
+  it('verifies login creates correct action with return value', () => {
+    const returnData = { type: LOGIN, user : { email: 'test@test.com', password: 'passwordTest' } };
+    const action = login('test@test.com', 'passwordTest')
+    expect(action).toEqual(expect.objectContaining(returnData))
+  });
+
+  it('verifies logout creates correct action with return value', () => {
+    const returnData = { type: LOGOUT };
+    const action = logout()
+    expect(action).toEqual(expect.objectContaining(returnData))
+  });
+
+  it('verifies displayNotificationDrawer creates correct action with return value', () => {
+    const returnData = { type: DISPLAY_NOTIFICATION_DRAWER };
+    const action = displayNotificationDrawer()
+    expect(action).toEqual(expect.objectContaining(returnData))
+  });
+
+  it('verifies hideNotificationDrawer creates correct action with return value', () => {
+    const returnData = { type: HIDE_NOTIFICATION_DRAWER };
+    const action = hideNotificationDrawer()
+    expect(action).toEqual(expect.objectContaining(returnData))
+  });
+})
